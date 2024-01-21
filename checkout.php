56fa247d49ef20aa8ce3689027abad12c3b87a41
towhid0845 <towhid0845@gmail.com>
2024-01-21 17:37:29 +0600

    last commit

--- checkout.php
+++ checkout.php
@@ -17,17 +17,17 @@ include("layout/head.php");
   include("layout/header.php");
   ?>
   <!-- heade & navbar part end -->
-   <!-- breadcrumb part start -->
-   <section class="breadcrumb pt-3 pt-md-5 pb-3">
+  <!-- breadcrumb part start -->
+  <section class="breadcrumb pt-3 pt-md-5 pb-3">
     <div class="container">
-    <div class="text">
-      <ul class="list-unstyled">
-        <li>
-          <a href="index.php"><i class="fas fa-home"></i> Home</a>
-        </li>
-        <li>Current Page</li>
-      </ul>
-    </div>
+      <div class="text">
+        <ul class="list-unstyled">
+          <li>
+            <a href="index.php"><i class="fas fa-home"></i> Home</a>
+          </li>
+          <li>Current Page</li>
+        </ul>
+      </div>
     </div>
   </section>
   <!-- breadcrumb part end -->
@@ -36,6 +36,7 @@ include("layout/head.php");
   <div class="wrapper">
     <section class="checkout px-4 px-sm-0">
       <div class="container">
+
         <div class="row">
           <div class="col-lg-12">
             <div class="isCustomer">
@@ -50,10 +51,12 @@ include("layout/head.php");
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="billing-section">
-              <form action="#">
+
+        <form action="">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="billing-section">
+                <!-- <form action="#"> -->
                 <h6>Personal Information</h6>
                 <div class="row">
                   <div class="col-md-12">
@@ -91,119 +94,121 @@ include("layout/head.php");
                   <i class="fa-solid fa-pencil"></i>
                   <textarea name="message" placeholder="Notes about your order, e.g. special notes for delivery."></textarea>
                 </div>
-              </form>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="title2 pt-5">
-                  <h2>Your order</h2>
-                </div>
-                <div class="order-table">
-                  <table class="table table-borderless">
-                    <thead>
-                      <tr>
-                        <th>PRODUCT</th>
-                        <th class="">TOTAL</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td class="prd-name">Broccoli Crowns <span>(×1)</span></td>
-                        <th>$10.00</th>
-                      </tr>
-                      <tr>
-                        <td class="prd-name">Crock Pot Slow Cooker <span>(×1)</span></td>
-                        <th>$3.99</th>
-                      </tr>
-                      <tr>
-                        <td class="prd-name">Saute Pan Silver <span>(×1)</span></td>
-                        <th>$1.90</th>
-                      </tr>
-                    </tbody>
-                    <tfoot>
-                      <tr>
-                        <th>Subtotal</th>
-                        <th>$15.89</th>
-                      </tr>
-                      <tr>
-                        <td>
-                          <span>Shipping</span>
-                          <ul>
-                            <li>Flat rate</li>
-                          </ul>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th>Total</th>
-                        <th class="total-price">$15.89</th>
-                      </tr>
-                    </tfoot>
-                  </table>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="checkout-payment-method mt-50">
-                  <div class="title2">
-                    <h2>Payment Method</h2>
-                  </div>
-                  <div id="checkout_accordion_1">
-                    <!-- card -->
-                    <div class="card">
-                      <h5 class="collapsed card-title d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#faq-item-2-4" aria-expanded="false">
-                        <i data-feather="circle"></i>
-                        <span>Bank</span>
-                        <img src="images/banking.png" alt="#">
-                      </h5>
-                      <div id="faq-item-2-4" class="collapse" data-parent="#checkout_accordion_1">
-                        <div class="card-body">
-                          <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- card -->
-                    <div class="card">
-                      <h5 class="card-title d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#faq-item-2-2" aria-expanded="true">
-                        <i data-feather="circle"></i>
-                        <i class="fa-solid fa-circle"></i>
-                        <span>Cash on delivery</span> <img class="img-fluid" src="./images/taka.png" alt="#">
-                      </h5>
-                      <div id="faq-item-2-2" class="collapse show" data-parent="#checkout_accordion_1">
-                        <div class="card-body">
-                          <p>Pay with cash upon delivery.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- card -->
-                    <div class="card">
-                      <h5 class="collapsed card-title d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#faq-item-2-3" aria-expanded="false">
-                        <i data-feather="circle"></i>
-                        <span>Mobile Banking</span> <img src="images/bKash.png" alt="#"><img src="images/nagad.png" alt="#"><img src="images/rocket.PNG" alt="#">
-                      </h5>
-                      <div id="faq-item-2-3" class="collapse" data-parent="#checkout_accordion_1">
-                        <div class="card-body">
-                          <p>Apple Pay is the modern way to pay.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- card -->
-                  </div>
-                </div>
+                <!-- </form> -->
               </div>
             </div>
-          </div>
-        </div>
-        <div class="row pb-5">
-          <div class="col-12 col-lg-6">
-            <div class="payment-note mt-5 mb-3">
-              <p>Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our privacy policy.</p>
+            <div class="col-lg-6">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="title2 pt-5">
+                    <h2>Your order</h2>
+                  </div>
+                  <div class="order-table">
+                    <table class="table table-borderless">
+                      <thead>
+                        <tr>
+                          <th>PRODUCT</th>
+                          <th class="">TOTAL</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td class="prd-name">Broccoli Crowns <span>(×1)</span></td>
+                          <th>$10.00</th>
+                        </tr>
+                        <tr>
+                          <td class="prd-name">Crock Pot Slow Cooker <span>(×1)</span></td>
+                          <th>$3.99</th>
+                        </tr>
+                        <tr>
+                          <td class="prd-name">Saute Pan Silver <span>(×1)</span></td>
+                          <th>$1.90</th>
+                        </tr>
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <th>Subtotal</th>
+                          <th>$15.89</th>
+                        </tr>
+                        <tr>
+                          <td>
+                            <span>Shipping</span>
+                            <ul>
+                              <li>Flat rate</li>
+                            </ul>
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Total</th>
+                          <th class="total-price">$15.89</th>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="checkout-payment-method mt-50">
+                    <div class="title2">
+                      <h2>Payment Method</h2>
+                    </div>
+                    <div id="checkout_accordion_1">
+                      <!-- card -->
+                      <div class="card">
+                        <h5 class="collapsed card-title d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#faq-item-2-4" aria-expanded="false">
+                          <i data-feather="circle"></i>
+                          <span>Bank</span>
+                          <img src="images/banking.png" alt="#">
+                        </h5>
+                        <div id="faq-item-2-4" class="collapse" data-parent="#checkout_accordion_1">
+                          <div class="card-body">
+                            <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.</p>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- card -->
+                      <div class="card">
+                        <h5 class="card-title d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#faq-item-2-2" aria-expanded="true">
+                          <i data-feather="circle"></i>
+                          <i class="fa-solid fa-circle"></i>
+                          <span>Cash on delivery</span> <img class="img-fluid" src="./images/taka.png" alt="#">
+                        </h5>
+                        <div id="faq-item-2-2" class="collapse show" data-parent="#checkout_accordion_1">
+                          <div class="card-body">
+                            <p>Pay with cash upon delivery.</p>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- card -->
+                      <div class="card">
+                        <h5 class="collapsed card-title d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#faq-item-2-3" aria-expanded="false">
+                          <i data-feather="circle"></i>
+                          <span>Mobile Banking</span> <img src="images/bKash.png" alt="#"><img src="images/nagad.png" alt="#"><img src="images/rocket.PNG" alt="#">
+                        </h5>
+                        <div id="faq-item-2-3" class="collapse" data-parent="#checkout_accordion_1">
+                          <div class="card-body">
+                            <p>Apple Pay is the modern way to pay.</p>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- card -->
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="checkout-bill">
-              <a class="call-btn" href="invoice.php">Place Order</a>
+          </div>
+
+          <div class="row pb-5">
+            <div class="col-12 col-lg-6">
+              <div class="payment-note mt-5 mb-3">
+                <p>Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our privacy policy.</p>
+              </div>
+              <div class="checkout-bill">
+                <a class="call-btn" href="invoice.php">Place Order</a>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   </div>
